5bd783aa9f90a262151ca2f2297a3a1675af1140
MouldiAchouri <achourimouldi2@gmail.com>
2025-11-03 13:38:16 +0100

    Exercice2(NotVue)

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,11 @@
-<script setup></script>
+<script setup>
+import socksBlueImage from './assets/images/socks_blue.jpeg'
+
+const productImage = socksBlueImage
+</script>
 
 <template>
-  <div class="product-display">
+<div class="product-display">
     <div class="product-container">
       <div class="product-info">
         <h1>Socks</h1>
