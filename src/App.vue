6e1f8342e4cb3ec077331c265f6ba527b52dddc5
MouldiAchouri <achourimouldi2@gmail.com>
2025-11-03 14:13:21 +0100

    Exercice3 v-if, v-else, v-show

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,11 @@
-<script setup>
+<script setup >
+import {ref} from 'vue'
 import socksBlueImage from './assets/images/socks_blue.jpeg'
 
 const productImage = socksBlueImage
+const inventory = 10
+const onSale = ref(true)
+
 </script>
 
 <template>
@@ -9,6 +13,10 @@ const productImage = socksBlueImage
     <div class="product-container">
       <div class="product-info">
         <h1>Socks</h1>
+      <p v-if="inventory > 10">En stock</p>
+      <p v-else-if ="inventory >= 1 && inventory <=10 ">faible stock</p>
+      <p v-else> rupture de stock</p>
+      <p v-show="onSale">En Solde !</p>
       </div>
     </div>
   </div>
